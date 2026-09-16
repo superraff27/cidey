612bc25890248f0dc4c2a38f4ddd413e794e725e
mochr4lovedulv <secretoclass@gmail.com>
2026-09-16 22:30:53 +0700

    update kode

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,5 +1,7 @@
 'use client';
 
+export const runtime = 'edge';
+
 import { useEffect, useState, useRef } from 'react';
 import Script from 'next/script';
 
--- app/api/proxy-video/route.js
+++ app/api/proxy-video/route.js
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 import { redis } from '@/lib/redis';
 
 export const dynamic = 'force-dynamic';
